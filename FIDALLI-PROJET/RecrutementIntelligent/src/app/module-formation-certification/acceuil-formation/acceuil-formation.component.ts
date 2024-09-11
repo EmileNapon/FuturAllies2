ce63f24e04be792fcb2aa491ef301edbe17b78dc
EmileNapon <emilenapon0@gmail.com>
2024-09-11 14:34:37 +0000

    first commit

---
+++ FIDALLI-PROJET/RecrutementIntelligent/src/app/module-formation-certification/acceuil-formation/acceuil-formation.component.ts
@@ -0,0 +1,26 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-acceuil-formation',
+  templateUrl: './acceuil-formation.component.html',
+  styleUrls: ['./acceuil-formation.component.css']
+})
+export class AcceuilFormationComponent {
+
+  isFixed: boolean = false;
+
+
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollOffset = 20;  // Distance de défilement avant que la navbar ne devienne fixe
+
+    // Ajouter la classe fixed si l'utilisateur a défilé au-delà du seuil
+    if (window.pageYOffset > scrollOffset) {
+      this.isFixed = true;
+    } else {
+      this.isFixed = false;
+    }
+  }
+
+}
