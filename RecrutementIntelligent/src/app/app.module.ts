00aa53a59edc2b65763038270d994c3e9dda1fc5
EmileNapon <emilenapon0@gmail.com>
2024-10-01 19:44:12 +0000

    first commit

---
+++ RecrutementIntelligent/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ModuleFormationCertificationModule } from './module-formation-certification/module-formation-certification.module';
+import { RouterModule } from '@angular/router';
+import { PremiumEtudiantComponent } from './gestion-abonnements-premium/premium-etudiant/premium-etudiant.component';
+import { GestionUtilisateursModule } from './gestion-utilisateurs/gestion-utilisateurs.module';
+import { AcceuilFormationComponent } from './module-formation-certification/acceuil-formation/acceuil-formation.component';
+import { OrientationAcceuilComponent } from './orientations/orientation-acceuil/orientation-acceuil.component';
+import { EtablissementsComponent } from './orientations/etablissements/etablissements.component';
+import { OrientationsModule } from './orientations/orientations.module';
+import { CardModuleComponent } from './module-formation-certification/domaines-listes/domaines.component';
+import { GestionnaireModule } from './gestionnaire/gestionnaire.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcceuilComponent,PremiumEtudiantComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, RouterModule, GestionUtilisateursModule, OrientationsModule, ModuleFormationCertificationModule, GestionnaireModule, 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
